078c25142cc14ae0ff2573e1f3ba0f4e682ba73f
Rodrigo Mello <rodrigo@localhost.localdomain>
2016-08-08 13:13:49 -0300

    Insert de novo anfitrião funcional mas sem rest

--- website/sendnew-teste.php
+++ website/sendnew-teste.php
@@ -1,59 +1,96 @@
 <?php
 	$dbh = new PDO('pgsql:host=localhost user=rodrigo dbname=evs password=123456');
 
-	// $stmt = $dbh->prepare('INSERT INTO ponto_de_venda (
-	// 	id_classe_ponto_de_venda,
-	// 	id_uf,
-	// 	id_anfitriao,
-	// 	nome,
-	// 	endereco,
-	// 	telefone,
-	// 	site,
-	// 	data_expiracao,
-	// 	dta,
-	// 	id_anfitriao_master
-	// ) VALUES (
-	// 	:id_classe_ponto_de_venda,
-	// 	:id_uf,
-	// 	:id_anfitriao,
-	// 	:nome,
-	// 	:endereco,
-	// 	:telefone,
-	// 	:site,
-	// 	:data_expiracao,
-	// 	:dta,
-	// 	:id_anfitriao_master
-	// )');
-	//
-	// $id_classe_ponto_de_venda = '1';
-	// $id_uf = '25';
-	// $id_anfitriao = '2';
-	// $nome = 'PHP User 01';
-	// $endereco = 'Rua Teste, 123';
-	// $telefone = '11999999999';
-	// $site = 'http://site.com';
-	// $data_expiracao = '2222-01-01';
-	// $dta = '2016-08-04 17:26:21';
-	// $id_anfitriao_master = '2';
-	//
-	// $stmt->bindParam('id_classe_ponto_de_venda', $id_classe_ponto_de_venda);
-	// $stmt->bindParam('id_uf', $id_uf);
-	// $stmt->bindParam('id_anfitriao', $id_anfitriao);
-	// $stmt->bindParam('nome', $nome);
-	// $stmt->bindParam('endereco', $endereco);
-	// $stmt->bindParam('telefone', $telefone);
-	// $stmt->bindParam('site', $site);
-	// $stmt->bindParam('data_expiracao', $data_expiracao);
-	// $stmt->bindParam('dta', $dta);
-	// $stmt->bindParam('id_anfitriao_master', $id_anfitriao_master);
+	// Ponto de venda ----------------------------------------------------------
 
-	// if ($stmt->execute()) {
-	// 	echo 'true';
-	// } else {
-	// 	echo 'false';
-	// }
+	$id_classe_ponto_de_venda = '1';
+	$id_uf = '25';
+	// $id_anfitriao = '2'; // duplicidade
+	// $nome = 'PHP User 01'; // duplicidade
+	// $endereco = 'Rua Teste, 123'; // duplicidade
+	$telefone = '11999999999';
+	$site = 'http://site.com';
+	$data_expiracao = '2222-01-01';
+	// $dta = '2016-08-04 17:26:21'; // duplicidade
+	$id_anfitriao_master = '2';
+
+	// Anfitrião ---------------------------------------------------------------
+
+	// $id_anfitriao = '11'; //duplicidade
+	$id_ponto_de_venda = '1';
+	$username = $username_canonical = 'webmaster2';
+	$email = $email_canonical = 'webmaster2@cm2tech.com.br';
+	$enabled = 't';
+	$salt = '37hnsd90fgn3hfdcgh26492jnsge23h';
+	$password = 'calfat@109{37hnsd90fgn3hfdcgh26492jnsge23h}';
+	// $last_login = '';
+	$locked = 'f';
+	$expired = 'f';
+	// $expires_at = 'teste vázio 03';
+	$confirmation_token = 'teste vázio 04';
+	// $password_requested_at = 'teste vázio 05';
+	$roles = 'tipo json';
+	$credentials_expired = 'f';
+	// $credentials_expire_at = 'teste vázio 06';
+	// $nome = 'Webmaster'; // duplicidade
+	// $endereco = 'Rua Comendador Miguel Calfat, 109'; // duplicidade
+	$faixa = '25';
+	$telefone = 'teste vázio 08';
+	$cpf = 'teste vázio 09';
+	// $dta = '2016-03-30 16:31:51'; //duplicidade
+
+	// Ambos -------------------------------------------------------------------
+
+	$dta = '2016-03-30 16:31:51';
+	$nome = 'nome';
+	$id_anfitriao = '' // TODO gerado dinamicamente
+	$endereco = 'Rua Comendador Miguel Calfat, 109';
+
+	// -------------------------------------------------------------------------
+
+	$stmt = $dbh->prepare('INSERT INTO ponto_de_venda (
+		id_classe_ponto_de_venda,
+		id_uf,
+		id_anfitriao,
+		nome,
+		endereco,
+		telefone,
+		site,
+		data_expiracao,
+		dta,
+		id_anfitriao_master
+	) VALUES (
+		:id_classe_ponto_de_venda,
+		:id_uf,
+		:id_anfitriao,
+		:nome,
+		:endereco,
+		:telefone,
+		:site,
+		:data_expiracao,
+		:dta,
+		:id_anfitriao_master
+	)');
+
+	$stmt->bindParam('id_classe_ponto_de_venda', $id_classe_ponto_de_venda);
+	$stmt->bindParam('id_uf', $id_uf);
+	$stmt->bindParam('id_anfitriao', $id_anfitriao);
+	$stmt->bindParam('nome', $nome);
+	$stmt->bindParam('endereco', $endereco);
+	$stmt->bindParam('telefone', $telefone);
+	$stmt->bindParam('site', $site);
+	$stmt->bindParam('data_expiracao', $data_expiracao);
+	$stmt->bindParam('dta', $dta);
+	$stmt->bindParam('id_anfitriao_master', $id_anfitriao_master);
+
+	if ($stmt->execute()) {
+		echo 'true';
+	} else {
+		echo 'false';
+	}
 
 	$stmt = $dbh->prepare('INSERT INTO anfitriao (
+		id_anfitriao,
 		id_ponto_de_venda,
 		username,
 		username_canonical,
@@ -62,15 +99,11 @@
 		enabled,
 		salt,
 		password,
-		last_login,
 		locked,
 		expired,
-		expires_at,
 		confirmation_token,
-		password_requested_at,
 		roles,
 		credentials_expired,
-		credentials_expire_at,
 		nome,
 		endereco,
 		faixa,
@@ -78,6 +111,7 @@
 		cpf,
 		dta
 	) VALUES (
+		:id_anfitriao,
 		:id_ponto_de_venda,
 		:username,
 		:username_canonical,
@@ -86,15 +120,11 @@
 		:enabled,
 		:salt,
 		:password,
-		:last_login,
 		:locked,
 		:expired,
-		:expires_at,
 		:confirmation_token,
-		:password_requested_at,
 		:roles,
 		:credentials_expired,
-		:credentials_expire_at,
 		:nome,
 		:endereco,
 		:faixa,
@@ -103,30 +133,7 @@
 		:dta
 	)');
 
-	$id_ponto_de_venda = '';
-	$username = '';
-	$username_canonical = '';
-	$email = '';
-	$email_canonical = '';
-	$enabled = '';
-	$salt = '';
-	$password = '';
-	$last_login = '';
-	$locked = '';
-	$expired = '';
-	$expires_at = '';
-	$confirmation_token = '';
-	$password_requested_at = '';
-	$roles = '';
-	$credentials_expired = '';
-	$credentials_expire_at = '';
-	$nome = '';
-	$endereco = '';
-	$faixa = '';
-	$telefone = '';
-	$cpf = '';
-	$dta = '';
-
+	$stmt->bindParam(':id_anfitriao', $id_anfitriao);
 	$stmt->bindParam(':id_ponto_de_venda', $id_ponto_de_venda);
 	$stmt->bindParam(':username', $username);
 	$stmt->bindParam(':username_canonical', $username_canonical);
@@ -135,15 +142,15 @@
 	$stmt->bindParam(':enabled', $enabled);
 	$stmt->bindParam(':salt', $salt);
 	$stmt->bindParam(':password', $password);
-	$stmt->bindParam(':last_login', $last_login);
+	// $stmt->bindParam(':last_login', $last_login);
 	$stmt->bindParam(':locked', $locked);
 	$stmt->bindParam(':expired', $expired);
-	$stmt->bindParam(':expires_at', $expires_at);
+	// $stmt->bindParam(':expires_at', $expires_at);
 	$stmt->bindParam(':confirmation_token', $confirmation_token);
-	$stmt->bindParam(':password_requested_at', $password_requested_at);
+	// $stmt->bindParam(':password_requested_at', $password_requested_at);
 	$stmt->bindParam(':roles', $roles);
 	$stmt->bindParam(':credentials_expired', $credentials_expired);
-	$stmt->bindParam(':credentials_expire_at', $credentials_expire_at);
+	// $stmt->bindParam(':credentials_expire_at', $credentials_expire_at);
 	$stmt->bindParam(':nome', $nome);
 	$stmt->bindParam(':endereco', $endereco);
 	$stmt->bindParam(':faixa', $faixa);
